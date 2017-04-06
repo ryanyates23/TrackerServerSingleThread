83df269f117f91b55339a8416b5545193231bd7e
Ryan Yates <ryanyates23@gmail.com>
2017-04-06 14:46:47 +0100

    initial commit

---
+++ TrackerServerSingleThread/kernels.cl
@@ -0,0 +1,264 @@
+__kernel void SobelEdge(__global uchar* in, __global uchar* out, const int width, const int height)
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y*width + x;
+    uchar data[9];
+    float dx = 0;
+    float dy = 0;
+    float dout = 0;
+    
+    if(x > 1 && x < width-2 && y > 1 && y < height-2)
+    {
+        data[0] = in[offset-width-1];
+        data[1] = in[offset-width];
+        data[2] = in[offset-width+1];
+        data[3] = in[offset-1];
+        data[4] = in[offset];
+        data[5] = in[offset+1];
+        data[6] = in[offset+width-1];
+        data[7] = in[offset+width];
+        data[8] = in[offset+width+1];
+        
+        dx = -data[0] + data[2] - 2*data[3] + 2*data[5] - data[6] + data[8];
+        dy = -data[0] - 2*data[1] - data[2] + data[6] + 2*data[7] + data[8];
+        
+        dout = sqrt(dx*dx + dy*dy);
+//        dout = dy;
+        
+        if(dout > 255) out[offset] = 255;
+        else if(dout < 0) out[offset] = 0;
+        else out[offset] = convert_uchar(dout);
+        
+        //out[offset] = in[offset];
+    }
+    else out[offset] = 0;
+    
+
+}
+
+__kernel void MeanFilter(__global uchar* in, __global uchar* out, const int width, const int height)
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y*width + x;
+    float mean = 0;
+    float sum = 0;
+    
+    if(x > 1 && x < width-2 && y > 1 && y < height-2)
+    {
+        sum = in[offset-width-1] + in[offset-width] + in[offset-width+1] + in[offset-1] + in[offset] + in[offset+1] + in[offset+width-1] + in[offset+width] + in[offset+width+1];
+        mean = sum/9;
+        
+        out[offset] = convert_uchar(mean);
+        
+    }
+    else
+    {
+        out[offset] = in[offset];
+    }
+}
+
+__kernel void MedianFilter3(__global uchar* in, __global uchar* out, const int width, const int height)
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y*width + x;
+    uchar data[9];
+    uchar temp;
+    int swapped = 1;
+    int i;
+    
+    if(x > 1 && x < width-2 && y > 1 && y < height-2)
+    {
+        data[0] = in[offset-width-1];
+        data[1] = in[offset-width];
+        data[2] = in[offset-width+1];
+        data[3] = in[offset-1];
+        data[4] = in[offset];
+        data[5] = in[offset+1];
+        data[6] = in[offset+width-1];
+        data[7] = in[offset+width];
+        data[8] = in[offset+width+1];
+        
+        while(swapped == 1)
+        {
+            swapped = 0;
+            for(i=0;i<7;i++)
+            {
+                if(data[i] > data[i+1])
+                {
+                    temp = data[i];
+                    data[i] = data[i+1];
+                    data[i+1] = temp;
+                    swapped = 1;
+                }
+            }
+        }
+        out[offset] = data[4];
+    }
+    else
+    {
+        out[offset] = in[offset];
+    }
+    
+}
+
+__kernel void MedianFilter5(__global uchar* in, __global uchar* out, const int width, const int height)
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y*width + x;
+    uchar data[25];
+    uchar temp;
+    int swapped = 1;
+    int i;
+    
+    if(x > 1 && x < width-2 && y > 1 && y < height-2)
+    {
+        data[0] = in[offset-2*width-2];
+        data[1] = in[offset-2*width-1];
+        data[2] = in[offset-2*width];
+        data[3] = in[offset-2*width+1];
+        data[4] = in[offset-2*width+2];
+        data[5] = in[offset-width-2];
+        data[6] = in[offset-width-1];
+        data[7] = in[offset-width];
+        data[8] = in[offset-width+1];
+        data[9] = in[offset-width+2];
+        data[10] = in[offset-2];
+        data[11] = in[offset-1];
+        data[12] = in[offset];
+        data[13] = in[offset+1];
+        data[14] = in[offset+2];
+        data[15] = in[offset+width-2];
+        data[16] = in[offset+width-1];
+        data[17] = in[offset+width];
+        data[18] = in[offset+width+1];
+        data[19] = in[offset+width+2];
+        data[20] = in[offset+2*width-2];
+        data[21] = in[offset+2*width-1];
+        data[22] = in[offset+2*width];
+        data[23] = in[offset+2*width+1];
+        data[24] = in[offset+2*width+2];
+        
+        while(swapped == 1)
+        {
+            swapped = 0;
+            for(i=0;i<23;i++)
+            {
+                if(data[i] > data[i+1])
+                {
+                    temp = data[i];
+                    data[i] = data[i+1];
+                    data[i+1] = temp;
+                    swapped = 1;
+                }
+            }
+        }
+        out[offset] = data[12];
+    }
+    else
+    {
+        out[offset] = in[offset];
+    }
+}
+
+__kernel void Binarise(__global uchar* in, __global uchar* out, const int width, const int height, const uchar thresh)
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y*width + x;
+    
+    if(in[offset] > thresh)
+    {
+        //out[offset] = in[offset];
+        out[offset] = 255;
+    }
+    else
+    {
+        out[offset] = 0;
+    }
+    
+}
+
+__kernel void CombineImages(__global uchar* in1, __global uchar* in2, global uchar* out, const int width, const int height)
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y*width + x;
+    int temp;
+
+    
+    temp = in1[offset] + in2[offset];
+    if (temp > 255) out[offset] = 255; else out[offset] = temp;
+}
+
+__kernel void ExtractROI(__global uchar* frame_in,
+                         __global uchar* ROI_out,
+                         const int widthFrame,
+                         const int heightFrame,
+                         const int ROIx,
+                         const int ROIy,
+                         const int ROIwidth,
+                         const int ROIheight
+                         )
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y*widthFrame + x;
+    int ROIoffset;
+    
+    if(x >= ROIx && x <= ROIx + ROIwidth && y >= ROIy && y <= ROIy + ROIheight)
+    {
+        ROIoffset = (y-ROIy)*ROIwidth+(x-ROIx);
+        ROI_out[ROIoffset] = frame_in[offset];
+    }
+    
+    
+}
+
+__kernel void InsertROI(__global uchar* frame,
+                        __global uchar* ROI,
+                        __global uchar* out,
+                        const int ROIx,
+                        const int ROIy,
+                        const int ROIwidth,
+                        const int ROIheight,
+                        const int frameWidth,
+                        const int frameHeight
+                        )
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    
+    int offset = y * frameWidth + x;
+    int ROIoffset;
+    
+    if(x >= ROIx && x <= ROIx + ROIwidth && y >= ROIy && y <= ROIy + ROIheight)
+    {
+        ROIoffset = (y-ROIy)*ROIwidth + (x-ROIx);
+        out[offset] = 255;
+        //out[offset] = ROI[ROIoffset];
+        //out[offset] = frame[offset];
+    }
+    else
+    {
+        out[offset] = frame[offset];
+    }
+}
+
+
+
+
+
+
+
+
--- TrackerServerSingleThread/main.cpp
+++ TrackerServerSingleThread/main.cpp
@@ -1,15 +1,390 @@
-//
-//  main.cpp
-//  TrackerServerSingleThread
-//
-//  Created by Ryan Yates on 06/04/2017.
-//  Copyright © 2017 Ryan Yates. All rights reserved.
-//
+#define __CL_ENABLE_EXCEPTIONS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "cl.hpp"
+#include <stdio.h>
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include <chrono>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define WIDTH 1280
+#define HEIGHT 720
+
+//#define WIDTH 1024
+//#define HEIGHT 576
+
+//using namespace std;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    //-------------BASIC INIT--------------------//
+    //    int argc = 51717;
+    //    char *argv = "localhost";
+    int x, y;
+    //int displayFrame = 0;
+    int filterType = 3; //0 = none, 1 = 3x3mean, 2 = 3x3 median, 3 = 5x5 median, 4 = 3x3median 2 pass
+    int enSobelEdge = 1;
+    int enBinarise = 0;
+    int enCombine = 0;
+    int enCombineFiltering = 0;
+    int width = WIDTH;
+    int height = HEIGHT;
+    
+    int sum, max;
+    float mean, thresh;
+    float threshFactor = 0.25;
+    float fps;
+    
+    
+    
+    
+    //--------------OPENCV INIT---------------//
+    unsigned char receiveBuffer[WIDTH*HEIGHT];
+    unsigned char sendBuffer[WIDTH*HEIGHT];
+    int vecSize = WIDTH*HEIGHT;
+    std::vector<unsigned char> h_frame_in(vecSize);
+    std::vector<unsigned char> h_frame_out(vecSize);
+    
+    //--------------OPENCL INIT--------------------//
+    cl::Program program;
+    cl::Context context;
+    
+    //This section sets up a variable to accept platform info, then gets it
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    
+    //This selects the default platform
+    cl::Platform platform = platforms.front();
+    printf("Generated Platform\n");
+    
+    //This sets up a variable for the devices and then gets them
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    
+    //This selects the default device
+    cl::Device device = devices.front();
+    printf("Established Device\n");
+    
+    std::cout << device.getInfo<CL_DEVICE_TYPE>();
+    std::cout << CL_DEVICE_TYPE_GPU << std::endl;
+    std::cout << "CL_Image Support: " << device.getInfo<CL_DEVICE_IMAGE_SUPPORT>() << std::endl;
+    std::cout << "Number of Compute Units: " << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << std::endl;
+    std::cout << "Global Memory Size (MB): " <<device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>()/1000000 << std::endl;
+    std::cout << "Local Memory Size (KB): " << device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>()/1024 << std::endl;
+    std::cout << "Max Clock Frequency (MHz): " <<device.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>() << std::endl;
+    
+    //This reads in the kernel file
+    std::ifstream kernelFile("kernels.cl");
+    std::string src(std::istreambuf_iterator<char>(kernelFile), (std::istreambuf_iterator<char>()));
+    printf("Read Kernel File\n");
+    
+    //This turns the string input into a source for kernels
+    cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
+    
+    //This creates an OpenCL context and a program from the sources
+    context = cl::Context(device);
+    printf("Created Context\n");
+    program = cl::Program(context, sources);
+    
+    //This tries to build the kernels and outputs a build log if it fails miserably
+    try {
+        cl::Error err = program.build();
+        printf("Built Program\n");
+    }
+    catch (cl::Error& e)
+    {
+        if (e.err() == CL_BUILD_PROGRAM_FAILURE)
+        {
+            for (cl::Device dev : devices)
+            {
+                // Check the build status
+                cl_build_status status = program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(dev);
+                if (status != CL_BUILD_ERROR)
+                    continue;
+                
+                // Get the build log
+                std::string name = dev.getInfo<CL_DEVICE_NAME>();
+                std::string buildlog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dev);
+                std::cerr << "Build log for " << name << ":" << std::endl
+                << buildlog << std::endl;
+            }
+        }
+        else
+        {
+            throw e;
+        }
+        
+    }
+    
+    cl::CommandQueue queue(context, device);
+    
+    cl::Buffer d_frame_in(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(unsigned char) * h_frame_in.size(), h_frame_in.data());
+    cl::Buffer d_original_frame(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(unsigned char)*h_frame_in.size(), h_frame_in.data());
+    cl::Buffer d_frame_out(context, CL_MEM_READ_WRITE, sizeof(unsigned char)*h_frame_out.size());
+    
+    cl::Kernel meanKernel(program, "MeanFilter");
+    cl::Kernel medianKernel3(program, "MedianFilter3");
+    cl::Kernel medianKernel5(program, "MedianFilter5");
+    cl::Kernel sobelKernel(program, "SobelEdge");
+    cl::Kernel binKernel(program, "Binarise");
+    cl::Kernel combineKernel(program, "CombineImages");
+    
+    
+    //------------TCP SERVER INIT--------------//
+    int totalBytesRead;
+    int totalBytesWritten;
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    //char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd,
+                       (struct sockaddr *) &cli_addr,
+                       &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
+    
+    
+    
+    while(1)
+    {
+        
+        //--------------------TCP RECEIVE----------------//
+        bzero(receiveBuffer,vecSize);
+        totalBytesRead = 0;
+        while(totalBytesRead != vecSize)
+        {
+            n = (int)read(newsockfd,&receiveBuffer[totalBytesRead],vecSize - totalBytesRead);
+            if (n < 0) error("ERROR reading from socket");
+            
+            totalBytesRead += n;
+        }
+        //std::cout << "Frame Received" << std::endl;
+        
+        //---------------DISPLAY FRAME------------------//
+        
+        //----------------PROCESSING----------------------//
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+        
+        for (y = 0; y < HEIGHT; y++)
+        {
+            for (x = 0; x < WIDTH-1; x++)
+            {
+                //std::cout << "Accessing Element: " << (y*WIDTH+x) << std::endl;
+                h_frame_in[y*WIDTH + x] = receiveBuffer[y*WIDTH + x];
+            }
+        }
+        
+        
+        
+        queue.enqueueWriteBuffer(d_frame_in, CL_TRUE, 0, sizeof(unsigned char)*h_frame_in.size(), h_frame_in.data());
+        queue.enqueueWriteBuffer(d_original_frame, CL_FALSE, 0, sizeof(unsigned char)*h_frame_in.size(), h_frame_in.data());
+        
+        if (filterType == 0)
+        {
+            queue.enqueueWriteBuffer(d_frame_out, CL_TRUE, 0, sizeof(unsigned char)*h_frame_in.size(), h_frame_in.data());
+        }
+        
+        if (filterType == 1)
+        {
+            
+            meanKernel.setArg(0, d_frame_in);
+            meanKernel.setArg(1, d_frame_out);
+            meanKernel.setArg(2, width);
+            meanKernel.setArg(3, height);
+            
+            queue.enqueueNDRangeKernel(meanKernel, cl::NullRange, cl::NDRange(width, height));
+            
+            //queue.enqueueReadBuffer(d_frame_out, CL_TRUE, 0, sizeof(unsigned char)*h_frame_out.size(), h_frame_out.data());
+            //queue.finish();
+            //h_frame_in = h_frame_out;
+        }
+        else
+        {
+            //h_frame_out = h_frame_in;
+        }
+        if (filterType == 2)
+        {
+            
+            medianKernel3.setArg(0, d_frame_in);
+            medianKernel3.setArg(1, d_frame_out);
+            medianKernel3.setArg(2, width);
+            medianKernel3.setArg(3, height);
+            
+            queue.enqueueNDRangeKernel(medianKernel3, cl::NullRange, cl::NDRange(width, height),cl::NullRange, NULL, NULL);
+            
+            //queue.enqueueReadBuffer(d_frame_out, CL_TRUE, 0, sizeof(unsigned char)*h_frame_out.size(), h_frame_out.data());
+            queue.finish();
+            //h_frame_in = h_frame_out;
+        }
+        else
+        {
+            //h_frame_out = h_frame_in;
+        }
+        if(filterType == 3)
+        {
+            
+            medianKernel5.setArg(0, d_frame_in);
+            medianKernel5.setArg(1, d_frame_out);
+            medianKernel5.setArg(2, width);
+            medianKernel5.setArg(3, height);
+            
+            queue.enqueueNDRangeKernel(medianKernel5, cl::NullRange, cl::NDRange(width, height),cl::NullRange, NULL, NULL);
+            queue.finish();
+            if(enCombine == 1 && enCombineFiltering == 1)
+            {
+                queue.enqueueCopyBuffer(d_frame_out, d_original_frame, 0, 0, vecSize);
+            }
+        }
+        else
+        {
+            //h_frame_out = h_frame_in;
+        }
+        if (enSobelEdge == 1)
+        {
+            
+            sobelKernel.setArg(0, d_frame_out);
+            sobelKernel.setArg(1, d_frame_in);
+            sobelKernel.setArg(2, width);
+            sobelKernel.setArg(3, height);
+            
+            queue.enqueueNDRangeKernel(sobelKernel, cl::NullRange, cl::NDRange(width, height),cl::NullRange, NULL, NULL);
+            queue.finish();
+        }
+        else
+        {
+            h_frame_out = h_frame_in;
+        }
+        if(enBinarise == 1)
+        {
+            queue.enqueueReadBuffer(d_frame_in, CL_TRUE, 0, sizeof(unsigned char)*h_frame_in.size(), h_frame_in.data());
+            
+            sum = 0;
+            max = 0;
+            for (y = 0; y < height; y++)
+            {
+                for (x = 0; x < width; x++)
+                {
+                    sum += h_frame_in[y*width + x];
+                    if(h_frame_in[y*width + x] > max) max = h_frame_in[y*width + x];
+                }
+            }
+            
+            mean = sum/(width*height);
+            thresh = mean+((max-mean)*threshFactor);
+            
+            
+            binKernel.setArg(0, d_frame_in);
+            binKernel.setArg(1, d_frame_out);
+            binKernel.setArg(2, width);
+            binKernel.setArg(3, height);
+            binKernel.setArg(4, (unsigned char)thresh);
+            
+            queue.enqueueNDRangeKernel(binKernel, cl::NullRange, cl::NDRange(width, height),cl::NullRange, NULL, NULL);
+            queue.finish();
+        }
+        else
+        {
+            //h_frame_out = h_frame_in;
+        }
+        
+        if (enCombine == 1 && enBinarise == 1)
+        {
+            
+            combineKernel.setArg(0, d_frame_out);
+            combineKernel.setArg(1, d_original_frame);
+            combineKernel.setArg(2, d_frame_in);
+            combineKernel.setArg(3, width);
+            combineKernel.setArg(4, height);
+            
+            queue.enqueueNDRangeKernel(combineKernel, cl::NullRange, cl::NDRange(width, height),cl::NullRange, NULL, NULL);
+            queue.finish();
+        }
+        else if (enCombine == 1 && enBinarise == 0)
+        {
+            
+            combineKernel.setArg(0, d_frame_in);
+            combineKernel.setArg(1, d_original_frame);
+            combineKernel.setArg(2, d_frame_out);
+            combineKernel.setArg(3, width);
+            combineKernel.setArg(4, height);
+            
+            queue.enqueueNDRangeKernel(combineKernel, cl::NullRange, cl::NDRange(width, height),cl::NullRange, NULL, NULL);
+            queue.finish();
+        }
+        
+        if ((enBinarise == 0 && enCombine == 0) || (enBinarise == 1 && enCombine == 1))
+        {
+            queue.enqueueReadBuffer(d_frame_in, CL_TRUE, 0, sizeof(unsigned char)*h_frame_out.size(), h_frame_out.data());
+        }
+        else
+        {
+            queue.enqueueReadBuffer(d_frame_out, CL_TRUE, 0, sizeof(unsigned char)*h_frame_out.size(), h_frame_out.data());
+        }
+        
+        
+        
+        for (y = 0; y < HEIGHT; y++)
+        {
+            for (x = 0; x < WIDTH-1; x++)
+            {
+                sendBuffer[y*WIDTH + x] = h_frame_out[y*WIDTH + x];
+            }
+        }
+        
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
+        fps = 1000000/duration;
+        
+        std::cout << fps << std::endl;
+        
+        //----------------TCP Send------------------------//
+        totalBytesWritten = 0;
+        while(totalBytesWritten != vecSize)
+        {
+            n = (int)write(newsockfd,&sendBuffer[totalBytesWritten],vecSize);
+            //std::cout << "Bytes Written: " << n << std::endl;
+            if (n < 0)
+                error("ERROR writing to socket");
+            totalBytesWritten += n;
+        }
+        
+        
+        //------------------------------------------------//
+    }
+    close(newsockfd);
+    close(sockfd);
+    while(1) x = 0;
     return 0;
 }
